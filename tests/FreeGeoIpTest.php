ab300ae38fabb8ca578932d31cce423a6b7d061e
Pablo de la Peña <p@shedcollective.org>
2018-01-11 18:01:56 +0000

    Minor formatting fixes

--- tests/FreeGeoIpTest.php
+++ tests/FreeGeoIpTest.php
@@ -2,7 +2,7 @@
 
 namespace Nails\GeoIp\Driver;
 
-class NailsTest extends \PHPUnit_Framework_TestCase
+class FreeGeoIpTest extends \PHPUnit_Framework_TestCase
 {
     public function testFoo()
     {
